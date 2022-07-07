96686a561aa9f86d73ec6777b802eca86585ae88
Samuel Osei Adu <asamuel355@yahoo.com>
2022-07-07 22:07:57 +0000

    new vcnews

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useEffect} from 'react';
+import alanBtn from '@alan-ai/alan-sdk-web';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const alanKey = '';
+const App = () => {
+
+    useEffect(() => {
+        alanBtn({
+            key: alanKey,
+        })
+    }, [])
+
+    return ( 
+        <div className = "App">
+            <p>Here we go again with Allan AI</p>
+        </div>
+    );
 }
 
 export default App;
