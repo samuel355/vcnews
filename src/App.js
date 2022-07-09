a51df5965ed0000ad2fff94dc29a9e79c0aa1d0f
Samuel Osei Adu <asamuel355@yahoo.com>
2022-07-09 21:06:45 +0000

    updates;

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import React, {useEffect} from 'react';
 import alanBtn from '@alan-ai/alan-sdk-web';
 
-const alanKey = '';
+const alanKey = '716994185ae941b55c6098865ed851422e956eca572e1d8b807a3e2338fdd0dc/stage';
 const App = () => {
 
     useEffect(() => {
         alanBtn({
             key: alanKey,
+            onCommand: ({command, articles}) =>{
+                if(command === 'newHeadlines'){
+                    console.log(articles);
+                }
+            }
         })
     }, [])
 
